1eadb10df8c09ff5da379d9256a604a73ce0e388
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-06-13 20:30:00 -0500

    added validation

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,30 +3,32 @@ const { Comment, Pizza } = require('../models');
 const commentController = {
   // add comment to pizza
   addComment({ params, body }, res) {
-    console.log(body);
+    console.log(params);
     Comment.create(body)
-        .then(({ _id }) => {
+      .then(({ _id }) => {
         return Pizza.findOneAndUpdate(
-            { _id: params.pizzaId },
-            { $push: { comments: _id } },
-            { new: true }
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
         );
-        })
-        .then(dbPizzaData => {
+      })
+      .then(dbPizzaData => {
+        console.log(dbPizzaData);
         if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
         }
         res.json(dbPizzaData);
-        })
-        .catch(err => res.json(err));
+      })
+      .catch(err => res.json(err));
   },
 
+  // add reply to comment
   addReply({ params, body }, res) {
     Comment.findOneAndUpdate(
       { _id: params.commentId },
       { $push: { replies: body } },
-      { new: true }
+      { new: true, runValidators: true }
     )
       .then(dbPizzaData => {
         if (!dbPizzaData) {
@@ -36,7 +38,7 @@ const commentController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.json(err));
-    },
+  },
 
   // remove comment
   removeComment({ params }, res) {
@@ -60,17 +62,16 @@ const commentController = {
       })
       .catch(err => res.json(err));
   },
-
   // remove reply
-    removeReply({ params }, res) {
-      Comment.findOneAndUpdate(
-        { _id: params.commentId },
-        { $pull: { replies: { replyId: params.replyId } } },
-        { new: true }
-      )
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => res.json(err));
-    }
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  }
 };
 
 module.exports = commentController;
--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-
 const ReplySchema = new Schema(
   {
     // set custom id to avoid confusion with parent comment _id
@@ -10,10 +9,13 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -28,19 +30,23 @@ const ReplySchema = new Schema(
   }
 );
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
-  },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
-  }, 
-  replies: [ReplySchema]
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+      required: true
+    },
+    commentBody: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
   {
     toJSON: {
